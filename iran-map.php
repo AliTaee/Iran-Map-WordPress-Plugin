b644aa81dd8e27e3c456d0e6584ebd62ba142249
AliTaee <ali.taee.72@gmail.com>
2018-07-30 22:39:08 +0430

    Add top menu

--- iran-map.php
+++ iran-map.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
-Plugin Name: Iran Map
-Description: Interactive Iran Map, admin can link each city to spicific link. 
-Author: Ali Taee
-Author uri: www.alitaee.xyz
-Version: 0.1.0
-License:      GPL2
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  irmap
-Domain Path:  /languages
+Plugin Name:    Iran Map
+Description:    Interactive Iran Map, admin can link each city to spicific link. 
+Author:         Ali Taee
+Author uri:     www.alitaee.xyz
+Version:        0.1.0
+License:        GPL2
+License URI:    https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:    irmap
+Domain Path:    /languages
 */
 
 /*
@@ -33,3 +33,53 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 
 }
+
+function iranmap_display_settings_page ( ) {
+
+    // check if user allowed access
+    if ( ! current_user_can ( 'manage_options' ) ) return;
+
+    ?>
+
+    <div class="wrap">
+        <h1> <?php echo esc_html( get_admin_page_title ( ) ); // display plugin name as page title ?> </h1>
+
+        <?php // display form settings ?>
+        <form action="option.php" method="post">
+
+            <?php // using WordPress function
+
+                // out put security fields 
+                settings_fields( 'iranmap_options' );
+
+                // out put settings sections - menu slug
+                do_settings_sections( 'iranmap' );
+
+                // submit button
+                submit_button( );
+            ?>
+
+        </form>
+    </div>
+
+    <?php
+}
+
+
+
+// add top-level administrative menu
+function iranmap_add_top_level_menu ( ) {
+    
+    // add menu title, menu name, who can access, url, icon, order
+    add_menu_page( 
+        'Iran Map',
+        'Iran Map Settings',
+        'manage_options',
+        'iran-map',
+        'iranmap_display_settings_page',
+        'dashicons-location-alt',
+        null
+    );
+
+}
+add_action( 'admin_menu', 'iranmap_add_top_level_menu' );
